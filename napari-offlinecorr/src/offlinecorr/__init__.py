f4c61bf74e2c0792a3491f86f5be6e805c15865c
John Vusich <82066832+johnvusich@users.noreply.github.com>
2025-09-30 10:42:50 -0500

    Add module docstring and versioning information

--- napari-offlinecorr/src/offlinecorr/__init__.py
+++ napari-offlinecorr/src/offlinecorr/__init__.py
@@ -1,1 +1,9 @@
-# mark package
+"""
+napari-offlinecorr: FLM-TEM correlation plugin for napari
+"""
+
+from .correlation_widget import FLMTEMCorrelationWidget
+
+__version__ = "0.1.0"
+
+__all__ = ["FLMTEMCorrelationWidget"]
